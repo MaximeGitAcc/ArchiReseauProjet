93406aae908fce0f75efa22c61db290b8c6636cb
Porée Gaël <poreegael@gmail.com>
2025-03-05 16:23:46 +0100

    modif produits

--- div2.php
+++ div2.php
@@ -4,14 +4,12 @@
                 <img src="IA2.png" alt="Produit 1">
                 <div class="description_produit">
                     <p>Un produit alliant fiabilité et élégance, un outil design. C'est le MINIGARS </p>
-                    <button class="btn-hover">En savoir plus</button>
                 </div>
             </section>
             <section id="produit2">
                 <img src="IA3.png" alt="Produit 2">
                 <div class="description_produit">
                     <p>Une technologie avancée pour révolutionner votre quotidien...</p>
-                    <button class="btn-hover">Découvrir</button>
                 </div>
             </section>
         </div>
