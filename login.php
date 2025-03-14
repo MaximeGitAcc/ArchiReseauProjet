a83b5144b29be72e90877bf170bdfe87909c891a
GitBiggestRat <156087724+MaximeGitAcc@users.noreply.github.com>
2025-03-14 13:07:05 +0100

    Paf en boite

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
     // Vérification simplifiée (remplacer par une vraie vérification)
     if ($username == "admin" && $password == "password") {
         // Créer un cookie avec le nom d'utilisateur
-        setcookie("username", $username, time() + (7 * 24 * 60 * 60), "/"); // Le cookie expire dans 7 jours
+        setcookie("username", $username, time() + (7 * 24 * 60 * 60), "/");
         header("Location: index.php"); 
         exit();
     } else {
